9fe9e5e0147cc688d3da978f5ffce4bfd88e9058
rawadhossain <rawad.hossain00@gmail.com>
2024-11-27 02:50:14 +0600

    buttons icons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { Button } from './components/Button';
+import { PlusIcon } from './icons/PlusIcon';
+import { ShareIcon } from './icons/ShareIcon';
+import { Card } from './components/Card';
+
 function App() {
     return (
         <>
-            <div className="bg-gray-300">hi there</div>
+            <div className="p-4">
+                <div className="flex justify-end gap-4">
+                    <Button
+                        variant="primary"
+                        text="Add Content"
+                        startIcon={<PlusIcon />}
+                    ></Button>
+                    <Button
+                        variant="secondary"
+                        text="Share Brain"
+                        startIcon={<ShareIcon />}
+                    ></Button>
+                </div>
+                <div className="flex gap-4">
+                    <Card
+                        type="twitter"
+                        title="Game Post"
+                        link="https://x.com/RawadHossain/status/1826685122579759477"
+                    />
+
+                    <Card
+                        type="youtube"
+                        title="Veritasium"
+                        link="https://www.youtube.com/embed/zTDFhWWPZ4Q?si=8EOq9vMCYd9MpBlE"
+                    />
+                </div>
+            </div>{' '}
         </>
     );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,9 +8,17 @@ interface ButtonProps {
 
 const variantClasses = {
     primary: 'bg-purple-600 text-white',
-    secondary: 'bg-purple-200 text-purple-400',
+    secondary: 'bg-purple-200 text-purple-600',
 };
 
+const defaultStyles =
+    'px-4 py-2 rounded-md font-normal flex justify-center items-center gap-1.5';
+
 export function Button({ variant, text, startIcon }: ButtonProps) {
-    return <button></button>;
+    return (
+        <button className={variantClasses[variant] + ' ' + defaultStyles}>
+            {startIcon}
+            {text}
+        </button>
+    );
 }
