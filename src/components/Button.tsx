74e8d4809fe256dfac5efe046ce7954d6ebe41b7
rawadhossain <rawad.hossain00@gmail.com>
2024-11-26 23:51:12 +0600

    fe

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react';
+
+interface ButtonProps {
+    variant: 'primary' | 'secondary';
+    text: string;
+    startIcon: ReactElement;
+}
+
+const variantClasses = {
+    primary: 'bg-purple-600 text-white',
+    secondary: 'bg-purple-200 text-purple-400',
+};
+
+export function Button({ variant, text, startIcon }: ButtonProps) {
+    return <button></button>;
+}
